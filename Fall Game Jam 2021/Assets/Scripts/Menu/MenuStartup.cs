34d57a9694d32d16fd5f32590753eaa6a47824d3
KeeganIngersoll <36973212+Kingersoll@users.noreply.github.com>
2021-10-19 17:06:03 -0400

    play ui

---
+++ Fall Game Jam 2021/Assets/Scripts/Menu/MenuStartup.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuStartup : MonoBehaviour
+{
+    public AudioClip letterCrash;
+    [SerializeField]
+
+    string GameTitle;
+    private int titleLength;
+    string currentTitle = "";
+
+    public AudioSource ASource;
+    public TextMeshPro tmp;
+    void Start()
+    {
+        ASource = GetComponent<AudioSource>();
+        ASource.clip = letterCrash;
+        tmp = GetComponent<TextMeshPro>();
+        titleLength = GameTitle.Length;
+        StartCoroutine("LetterStamp");
+    }
+    IEnumerator LetterStamp()
+    {
+        print("thumo");
+        //wait initial time then slowly clomp in
+        yield return new WaitForSeconds(2.0f);
+     
+        for(int i = 0 ;  i < titleLength; i++)
+        {
+            yield return new WaitForSeconds(.25f);
+
+            ASource.Play();
+            string gs = GameTitle.Substring(0, i);
+
+           // tmp.text = gs;
+
+        }
+    }
+
+
+}
