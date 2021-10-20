cf19b8ef95fd494644bceabf130a65f874fe377f
Alice Ryan <caelstuffs@gmail.com>
2021-10-20 18:34:55 -0400

    Bug Dash Basic Completion

--- Fall Game Jam 2021/Assets/Scripts/BugDash.cs
+++ Fall Game Jam 2021/Assets/Scripts/BugDash.cs
@@ -7,28 +7,33 @@ public class BugDash : MonoBehaviour
     public bool isDash;
     public float dashTime;
     public float dashSpeed;
-
-    internal float DashMaxVel;
-    //add code that sets an alt max velocity during dash to keep movement from acheiving dash speeds but also avoid something from knocking a dashing bug to go EVEN FURTHER BEYOND
-
+    
+    internal PlayerController playCtrl;
 
     internal void Start()
     {
+        playCtrl = GetComponent<PlayerController>();
         isDash = false;
     }
 
     internal IEnumerator Nyoom()
     {
-        //add force to vector 2 velocity, not add force
-         
+        Vector3 velocity;
+        float baseMaxStor;
+
         isDash = true;
-        Vector2 velocity = (dashSpeed * transform.forward);
-        Debug.Log("velocity: " + velocity);
-        Vector3 realVelocity = new Vector3(velocity.x, 0, velocity.y); //Rearrange velocity to fit in world
-        Debug.Log("real velocity: " + realVelocity);
-        transform.Translate(realVelocity * Time.deltaTime); //Move bug by velocity (along x/y axis)
+
+        //match movement speed cap to speed of dash and store original value
+        baseMaxStor = playCtrl.baseMaxSpeed;
+        playCtrl.baseMaxSpeed = dashSpeed;
+
+        velocity = (dashSpeed * transform.forward);
+        velocity = new Vector3(velocity.x, 0, velocity.y); //Rearrange velocity to fit in world
+        
+        transform.Translate(velocity * Time.deltaTime); //Move bug by velocity (along x/y axis)
 
         yield return new WaitForSeconds(dashTime);
+        playCtrl.baseMaxSpeed = baseMaxStor;
         isDash = false;
     }
 }
