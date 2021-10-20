25f7e6c6d565b386f1f902324448c8e38be83d26
Alice Ryan <caelstuffs@gmail.com>
2021-10-20 10:41:16 -0400

    Bug Dash Player Controller Implementation
    Created Bug Dash Code and set up a boolean in it that enables or disables movement and rotation in player controller.

---
+++ Fall Game Jam 2021/Assets/BugDash.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugDash : MonoBehaviour
+{
+    public bool isDash;
+    
+    // Start is called before the first frame update
+    internal void Start()
+    {
+        isDash = false;
+    }
+
+    // Update is called once per frame
+    internal void Update()
+    {
+        
+    }
+}
